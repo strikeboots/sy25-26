473b538ea59b26661e157ed3c31d952e9c25e290
strikeboots <mateosydney@outlook.com>
2026-01-09 13:05:49 -0700

    change

--- pyfest/pyfest/pyfest.py
+++ pyfest/pyfest/pyfest.py
@@ -1,1 +1,15 @@
+# The initial lineup
+lineup = [
+    ("Code Play", "Indie", 30),
+    ("The Pythonistas", "Rock", 45),
+    ("Syntax Error", "Metal", 60)
+]
 
+# 1. Add the headliner
+headliner = ("The Byte Beats", "Synthwave", 90)
+lineup.append(headliner)
+print(lineup)
+remove_band = lineup.pop(1)
+lineup.append(remove_band)
+print(lineup)
+# Continue the logic below...
