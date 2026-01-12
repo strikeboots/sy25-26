69dea35b7bb05f7377c7cae86c602caa57f95447
strikeboots <mateosydney@outlook.com>
2026-01-12 13:05:57 -0700

    boiiiiii

--- pyfest/pyfest/pyfest.py
+++ pyfest/pyfest/pyfest.py
@@ -1,15 +1,8 @@
-# The initial lineup
-lineup = [
-    ("Code Play", "Indie", 30),
-    ("The Pythonistas", "Rock", 45),
-    ("Syntax Error", "Metal", 60)
-]
-
-# 1. Add the headliner
-headliner = ("The Byte Beats", "Synthwave", 90)
-lineup.append(headliner)
-print(lineup)
-remove_band = lineup.pop(1)
-lineup.append(remove_band)
-print(lineup)
-# Continue the logic below...
+print("---Py-Fest 2026 Stage Manager ---")
+print("1. View Lineup & Total Time")
+print("2. Add a New Band")
+print("3. Move First Band to End (Late Arrival)")
+print("4. Remove a Band by Name")
+print("5. Move Band to Specific Position")
+print("6. Exit")
+input("Select an option (1-6): ")
