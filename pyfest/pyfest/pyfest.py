16064dee64f34ff5984581b6ef55c1f1bf4c39e9
strikeboots <mateosydney@outlook.com>
2026-01-13 12:59:50 -0700

    tuesday update fr

--- pyfest/pyfest/pyfest.py
+++ pyfest/pyfest/pyfest.py
@@ -1,3 +1,5 @@
+print("Welcome to Py-Fest 2026 Stage Manager!")
+print("")
 print("---Py-Fest 2026 Stage Manager ---")
 print("1. View Lineup & Total Time")
 print("2. Add a New Band")
@@ -6,3 +8,14 @@ print("4. Remove a Band by Name")
 print("5. Move Band to Specific Position")
 print("6. Exit")
 input("Select an option (1-6): ")
+
+lineup = ["{'name': 'The Pythonistas', '(Rock)': 30},
+          {'name': 'Code Play', '(Indie)': 45},
+          {'name': 'Syntax Error', '(Metal)': 60}]"
+
+if choice == "1." or choice == "1":
+    total_time = sum(band['duration'] for band in lineup)
+    print("--- Current Lineup ---")
+    for band in lineup:
+        print(f"{band['name']} - {band['duration']} mins")
+    print(f"Total Performance Time: {total_time} mins")
